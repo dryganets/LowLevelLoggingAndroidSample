1dde9ae65cb17d7b13b8ddd51394065d906b8fdd
Sergei Dryganets <sergeyd@microsoft.com>
2018-07-30 16:43:47 -0700

    Added a log line.

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Button, Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +18,17 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  buttonOnPress = () => {
+    console.log('Logging sample ' + Date.now());
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button onPress= {this.buttonOnPress} title="Log it!" />
       </View>
     );
   }
